d89ea4bf741f119ad34b1b2be493d5eed1f1b133
kcolby1 <kcolby1@uncc.edu>
2023-04-25 19:53:46 -0400

    Web app commit

--- main.tf
+++ main.tf
@@ -34,11 +34,41 @@ resource "azurerm_resource_group" "rg" {
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+ name = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
+ resource_group_name = azurerm_resource_group.rg.name
+ location = azurerm_resource_group.rg.location
+ account_tier = "Standard"
+ account_replication_type = "LRS"
+ 
+ tags = local.tags
+}
 
-  tags = local.tags
+// Web App
+
+resource "azurerm_application_insights" "example" {
+ name = "workspace-example-ai-kcolby1"
+ location = azurerm_resource_group.rg.location
+ resource_group_name = azurerm_resource_group.rg.name
+ application_type = "web"
+}
+
+resource "azurerm_key_vault" "example" {
+ name = "kc-workspacevault-new1"
+ location = azurerm_resource_group.rg.location
+ resource_group_name = azurerm_resource_group.rg.name
+ tenant_id = data.azurerm_subscription.current.tenant_id
+ sku_name = "premium"
+}
+
+resource "azurerm_machine_learning_workspace" "example" {
+ name = "machine-learning-ws-kcolby"
+ location = azurerm_resource_group.rg.location
+ resource_group_name = azurerm_resource_group.rg.name
+ application_insights_id = azurerm_application_insights.example.id
+ key_vault_id = azurerm_key_vault.example.id
+ storage_account_id = azurerm_storage_account.storage.id
+
+ identity {
+ type = "SystemAssigned"
+ }
 }
