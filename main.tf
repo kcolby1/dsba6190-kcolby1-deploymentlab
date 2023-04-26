16db475eeeeb5c0c4b20c60e849761b69f8afc19
kcolby1 <kcolby1@uncc.edu>
2023-04-25 20:10:35 -0400

    Latest update to run

--- main.tf
+++ main.tf
@@ -34,42 +34,85 @@ resource "azurerm_resource_group" "rg" {
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
- name = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
- resource_group_name = azurerm_resource_group.rg.name
- location = azurerm_resource_group.rg.location
- account_tier = "Standard"
- account_replication_type = "LRS"
- 
- tags = local.tags
+  name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = local.tags
 }
 
 // Web App
 
 resource "azurerm_application_insights" "example" {
- name = "workspace-example-ai-kcolby1"
- location = azurerm_resource_group.rg.location
- resource_group_name = azurerm_resource_group.rg.name
- application_type = "web"
+  name                = "workspace-example-ai-kcolby1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
 }
 
 resource "azurerm_key_vault" "example" {
- name = "kc-workspacevault-new1"
- location = azurerm_resource_group.rg.location
- resource_group_name = azurerm_resource_group.rg.name
- tenant_id = data.azurerm_subscription.current.tenant_id
- sku_name = "premium"
+  name                = "kc-workspacevault-new1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_subscription.current.tenant_id
+  sku_name            = "premium"
 }
 
 resource "azurerm_machine_learning_workspace" "example" {
- name = "machine-learning-ws-kcolby"
- location = azurerm_resource_group.rg.location
- resource_group_name = azurerm_resource_group.rg.name
- application_insights_id = azurerm_application_insights.example.id
- key_vault_id = azurerm_key_vault.example.id
- storage_account_id = azurerm_storage_account.storage.id
+  name                    = "machine-learning-ws-kcolby"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  application_insights_id = azurerm_application_insights.example.id
+  key_vault_id            = azurerm_key_vault.example.id
+  storage_account_id      = azurerm_storage_account.storage.id
 
- identity {
- type = "SystemAssigned"
- }
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
+// AZ Cosmos
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "tfex-cosmos-db-${random_integer.deployment_id_suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = "westus"
+    failover_priority = 0
+  }
+}
